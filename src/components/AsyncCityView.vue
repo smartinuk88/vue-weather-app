d90c2dec93d2676e122828a128caf546f112aca3
Scott Martin <scott.martin88@live.com>
2025-08-19 08:42:48 +0200

    Calculate time offset

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -8,7 +8,19 @@ const getWeatherData = async () => {
     const weatherData = await axios.get(
       `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lon}&exclude={part}&units=metric&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}`,
     )
-    return weatherData
+
+    // Calc current date and time
+    const localOffset = new Date().getTimezoneOffset() * 60000
+    const utc = weatherData.data.current.dt * 1000 + localOffset
+    weatherData.data.currentTime = utc + 1000 * weatherData.data.timezone_offset
+
+    // Calc hourly weather offset
+    weatherData.data.hourly.forEach((hour) => {
+      const utc = hour.dt * 1000 + localOffset
+      hour.currentTime = utc + 1000 * weatherData.data.timezone_offset
+    })
+
+    return weatherData.data
   } catch (err) {
     console.log(err)
   }
@@ -20,8 +32,27 @@ console.log(weatherData)
 
 <template>
   <div class="flex flex-1 flex-col items-center">
+    <!-- Banner -->
     <div v-if="route.query.preview" class="p-4 w-full text-center">
       <p>You are currently previewing this city. Click the "+" to add to your cities.</p>
+    </div>
+    <!-- Weather Overview -->
+    <div class="flex flex-col items-center py-12">
+      <h1 class="text-4xl mb-2">{{ route.params.place }}</h1>
+      <p>
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString('en-gb', {
+            weekday: 'short',
+            day: '2-digit',
+            month: 'long',
+          })
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString('en-gb', {
+            timeStyle: 'short',
+          })
+        }}
+      </p>
     </div>
   </div>
 </template>
