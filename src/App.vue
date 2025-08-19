94c4996b85246a0e21540f20a5fb12e15ad8bfe7
Scott Martin <scott.martin88@live.com>
2025-08-19 09:29:19 +0200

    Build site navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import SiteNavigation from './components/SiteNavigation.vue'
 </script>
 
 <style lang="scss" scoped></style>
